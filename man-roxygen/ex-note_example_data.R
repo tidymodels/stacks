1aae233cdb7c7810c40c960ca5cc2376c84692cc
Simon P. Couch <simonpatrickcouch@gmail.com>
2020-06-17 12:56:48 -0700

    some poking around with automatic docs

---
+++ man-roxygen/ex-note_example_data.R
@@ -0,0 +1,5 @@
+#' # See \code{?example_data} to learn more about the
+#' # \code{svm_res_} and \code{spline_res_} objects exported
+#' # with the package. Long story short, they're example
+#' # \code{tune_results} objects outputted from [tune::tune_grid()]!
+#' @md
