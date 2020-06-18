0a25f0890847ef320824f20c2f44822e8e93f255
Simon P. Couch <simonpatrickcouch@gmail.com>
2020-06-18 13:30:47 -0700

    fix argument name in stack print method
    Adjusted the argument name but didn't transition the body from stack -> x.

--- R/print.R
+++ R/print.R
@@ -1,14 +1,14 @@
 #' @export
 print.stack <- function(x, ...) {
-  cat(glue::glue("# A model stack with {length(stack$members)} member",
-                 "{if (length(stack$members) != 1) 's' else ''}",
-                 "{if (length(stack$members) != 0) ':' else '.'}"))
-  if (length(stack$members) != 0) {cat("\n")}
+  cat(glue::glue("# A model stack with {length(x$members)} member",
+                 "{if (length(x$members) != 1) 's' else ''}",
+                 "{if (length(x$members) != 0) ':' else '.'}"))
+  if (length(x$members) != 0) {cat("\n")}
   
   members <- 
     purrr::map2(
-      unname(stack$members), 
-      names(stack$members), 
+      unname(x$members), 
+      names(x$members), 
       function(member, name) {
         cat(glue::glue(
             "#   {name}: ", 
@@ -18,9 +18,9 @@ print.stack <- function(x, ...) {
       }
     )
   
-  cat(glue::glue("# Outcome: {get_outcome(stack)}\n"))
+  cat(glue::glue("# Outcome: {get_outcome(x)}\n"))
   cat("\n")
   
-  cat(glue::glue("# Evaluated: {is_evaluated(stack)}"))
+  cat(glue::glue("# Evaluated: {is_evaluated(x)}"))
   cat("\n")
 }
