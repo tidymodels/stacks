fc67386c40f9d20d58be572c427de4512e88f4dc
Simon P. Couch <simonpatrickcouch@gmail.com>
2020-06-24 16:15:18 -0700

    update print method
    Will need to eventually map which model definitions map to which members more carefully.

--- R/print.R
+++ R/print.R
@@ -1,27 +1,27 @@
 #' @export
 print.stack <- function(x, ...) {
-  print(tibble::as_tibble(x))
-#   cat(glue::glue("# A model stack with {length(x$members)} member",
-#                  "{if (length(x$members) != 1) 's' else ''}",
-#                  "{if (length(x$members) != 0) ':' else '.'}"))
-#   if (length(x$members) != 0) {cat("\n")}
-# 
-#   members <-
-#     purrr::map2(
-#       unname(x$members),
-#       names(x$members),
-#       function(member, name) {
-#         cat(glue::glue(
-#             "#   {name}: ",
-#             "{length(unique(member$.predictions[[1]]$.config))} sub-models")
-#           )
-#         cat("\n")
-#       }
-#     )
-# 
-#   cat(glue::glue("# Outcome: {get_outcome(x)}\n"))
-#   cat("\n")
-# 
-#   cat(glue::glue("# Evaluated: {is_evaluated(x)}"))
-#   cat("\n")
+  n_members <- ncol(x) - 1
+  n_model_defs <- length(attr(x, "model_def_names"))
+  outcome_name <- colnames(x)[1]
+  
+  cat(glue::glue("# A model stack with {n_model_defs} model definition ",
+                 "{if (n_model_defs != 1) 's' else ''}",
+                 "and {n_members} member",
+                 "{if (n_members != 1) 's' else ''}",
+                 "{if (n_model_defs != 0) ':' else '.'}"))
+  if (n_model_defs != 0) {cat("\n")}
+  
+  n_by_model_defs <-
+    purrr::map(
+      attr(x, "model_def_names"),
+      function(name) {
+        cat(glue::glue(
+            "#   {name}: ",
+            "{sum(stringi::stri_detect_fixed(colnames(x), name))} sub-models")
+          )
+        cat("\n")
+      }
+    )
+
+  cat(glue::glue("# Outcome: {get_outcome(x)}\n"))
 }
