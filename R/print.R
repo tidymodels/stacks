3b125146b070e78b9810d471a197e007b5b2082d
Simon P. Couch <simonpatrickcouch@gmail.com>
2020-06-25 09:32:15 -0700

    fix print method and description

--- R/print.R
+++ R/print.R
@@ -1,11 +1,11 @@
 #' @export
 print.stack <- function(x, ...) {
-  n_members <- ncol(x) - 1
+  n_members <- if (ncol(x) == 0) {0} else {ncol(x) - 1}
   n_model_defs <- length(attr(x, "model_def_names"))
   outcome_name <- colnames(x)[1]
   
-  cat(glue::glue("# A model stack with {n_model_defs} model definition ",
-                 "{if (n_model_defs != 1) 's' else ''}",
+  cat(glue::glue("# A model stack with {n_model_defs} model definition",
+                 "{if (n_model_defs != 1) 's ' else ' '}",
                  "and {n_members} member",
                  "{if (n_members != 1) 's' else ''}",
                  "{if (n_model_defs != 0) ':' else '.'}"))
