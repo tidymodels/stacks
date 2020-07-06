ac7867a9bf9b3985a0f1d7e62ae5af60ca077208
Simon P. Couch <simonpatrickcouch@gmail.com>
2020-07-06 07:48:59 -0700

    restore control_stack_* functions
    Should be helpful even with the new functionality moved to tune—note that this depends on tune version from PR 242.

---
+++ R/control.R
@@ -0,0 +1,40 @@
+#' Control wrappers
+#'
+#' Supply these light wrappers as the `control` argument in a
+#' [tune::tune_grid], [tune::tune_bayes], or [tune::fit_resamples]
+#' call to return the needed workflow elements for use in a data stack.
+#' These functions will return the appropriate control grid to ensure that
+#' a minimal workflow giving details on model specifications and
+#' preprocessors is supplied in the resampling results object!
+#' 
+#' @return A [tune::control_grid], [tune::control_bayes], 
+#' or [tune::control_resamples] object.
+#' 
+#' @seealso See [example_data] for examples of these functions used in context.
+#'
+#' @rdname control_stack
+#' @export
+control_stack_grid <- function() {
+  tune::control_grid(
+    save_pred = TRUE,
+    save_workflow = TRUE
+  )
+}
+
+#' @rdname control_stack
+#' @export
+control_stack_resamples <- function() {
+  tune::control_resamples(
+    save_pred = TRUE,
+    save_workflow = TRUE
+  )
+}
+
+#' @rdname control_stack
+#' @export
+control_stack_bayes <- function() {
+  tune::control_bayes(
+    save_pred = TRUE,
+    save_workflow = TRUE
+  )
+}
