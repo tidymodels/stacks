6a4c68bf4e180f9c2d1340916c81ddc6b46e3c21
Simon P. Couch <simonpatrickcouch@gmail.com>
2020-07-16 12:32:40 -0700

    revise member prediction
    always start out with numeric/prob prediction from members, and then use a tidypredict-style prediction method

--- R/predict.R
+++ R/predict.R
@@ -54,22 +54,24 @@
 predict.model_stack <- function(object, new_data, type = NULL, members = FALSE, 
                                 opts = list(), ...) {
   type <- check_pred_type(object, type)
-  mode <- object[["mode"]]
-    
+  
   coefs <- 
     .get_glmn_coefs(object[["coefs"]][["fit"]]) %>%
     dplyr::select(terms, estimate)
   
-  do.call(
-    paste0("predict_", object[["mode"]], "_", type),
-    list(
+  member_preds <- 
+    rlang::call2(
+      paste0("predict_members_", object[["mode"]]),
       model_stack = object,
       coefs = coefs,
       new_data = new_data,
-      members = members,
       opts = opts
-    )
-  )
+    ) %>%
+    rlang::eval_tidy()
+  
+  # will now need to combine member predictions
+  
+  invisible(TRUE)
 }
 
 check_pred_type <- function(object, type) {
@@ -84,37 +86,7 @@ check_pred_type <- function(object, type) {
   type
 }
 
-predict_regression_numeric <- function(model_stack, coefs, new_data, members, 
-                                       opts) {
-  # predict using each member model
-  prediction_members <- 
-    predict_regression_numeric_members(model_stack, coefs, new_data, opts)
-  
-  term_coefs <-
-    coefs %>%
-    dplyr::filter(estimate !=0 & terms != "(Intercept)") %>%
-    tidyr::pivot_wider(values_from = estimate, names_from = terms)
-  
-  # multiply the predictions by the appropriate coefficients
-  predictions <- 
-    mapply("*", prediction_members, term_coefs) %>%
-    rowSums() %>%
-    `+`(
-      coefs %>%
-        dplyr::filter(terms == "(Intercept)") %>%
-        dplyr::pull()
-    ) %>%
-    tibble::as_tibble_col(".pred")
-  
-  if (members) {
-    predictions <-
-      dplyr::bind_cols(predictions, prediction_members)
-  }
-  
-  predictions
-}
-
-predict_regression_numeric_members <- function(model_stack, coefs, new_data, opts) {
+predict_members_regression <- function(model_stack, coefs, new_data, opts) {
   predictions <- 
     purrr::map(
       model_stack[["member_fits"]],
@@ -129,108 +101,7 @@ predict_regression_numeric_members <- function(model_stack, coefs, new_data, opt
   predictions
 }
 
-predict_classification_prob <- function(model_stack, coefs, new_data, members,
-                                        opts) {
-  cols_map_tibble <-
-    tibble::enframe(model_stack[["cols_map"]]) %>% 
-    tidyr::unnest(cols = value) %>%
-    dplyr::mutate(sanitize_classification_names(
-      model_stack, 
-      value
-    )) %>%
-    dplyr::select(
-      model = name,
-      member = new,
-      term = value
-    )
-  
-  term_coefs <-
-    coefs %>%
-    dplyr::filter(estimate !=0 & terms != "(Intercept)")
-  
-  prediction_members <- 
-    predict_classification_prob_members(
-      model_stack = model_stack,
-      coefs = coefs,
-      new_data = new_data,
-      opts = opts
-    )
-  
-  predictions <-
-    prediction_members %>%
-    tibble::rowid_to_column() %>%
-    tidyr::pivot_longer(c(dplyr::everything(), -rowid)) %>%
-    dplyr::left_join(cols_map_tibble, by = c("name" = "term")) %>%
-    dplyr::mutate(
-      pred_class = stringi::stri_replace_all_fixed(
-        name, paste0("_", member), ""
-      )
-    ) %>%
-    dplyr::inner_join(term_coefs, by = c("name" = "terms")) %>%
-    dplyr::mutate(weighted_est = value * estimate) %>%
-    dplyr::select(rowid, name, pred_class, weighted_est) %>%
-    dplyr::group_by(rowid, pred_class) %>%
-    dplyr::summarize(pred_class_sum = sum(weighted_est), .groups = "drop") %>%
-    dplyr::group_by(rowid) %>%
-    dplyr::mutate(pred_class_sum_norm = pred_class_sum / sum(pred_class_sum)) %>%
-    dplyr::ungroup() %>%
-    dplyr::select(-pred_class_sum) %>%
-    tidyr::pivot_wider(
-      id_cols = c(rowid), 
-      names_from = pred_class, 
-      values_from = pred_class_sum_norm
-    ) %>%
-    dplyr::arrange(rowid) %>%
-    dplyr::select(-rowid)
-  
-  if (members) {
-    predictions <-
-      dplyr::bind_cols(predictions, prediction_members)
-  }
-  
-  predictions
-}
-
-predict_classification_class <- function(model_stack, coefs, new_data, members,
-                                         opts) {
-  prediction_probs <- 
-    predict_classification_prob(
-      model_stack = model_stack,
-      coefs = coefs,
-      new_data = new_data,
-      members = FALSE,
-      opts = opts
-    )
-  
-  predictions <- 
-    prediction_probs %>%
-    tibble::rowid_to_column() %>%
-    tidyr::pivot_longer(cols = c(dplyr::everything(), -rowid)) %>%
-    dplyr::group_by(rowid) %>%
-    dplyr::filter(value == max(value)) %>%
-    dplyr::arrange(rowid) %>%
-    dplyr::pull(name) %>%
-    stringi::stri_replace_all_fixed(".pred_", "") %>%
-    tibble::as_tibble_col(".pred")
-  
-  if (members) {
-    predictions <-
-      dplyr::bind_cols(
-        predictions, 
-        predict_classification_class_members(
-          model_stack,
-          coefs,
-          new_data,
-          opts
-        )
-      )
-  }
-  
-  predictions
-}
-
-predict_classification_prob_members <- function(model_stack, coefs, new_data, 
-                                                opts) {
+predict_members_classification <- function(model_stack, coefs, new_data, opts) {
   purrr::map(
     model_stack[["member_fits"]],
     predict,
@@ -245,22 +116,6 @@ predict_classification_prob_members <- function(model_stack, coefs, new_data,
                        names_from = name,
                        values_from = 3:ncol(.)) %>%
     dplyr::select(-rowid)
-}
-
-predict_classification_class_members <- function(model_stack, coefs, new_data, 
-                                                 opts) {
-  predictions <- 
-    purrr::map(
-      model_stack[["member_fits"]],
-      predict,
-      new_data = new_data,
-      type = "class",
-      opts = opts,
-    ) %>%
-    purrr::map(dplyr::pull) %>%
-    tibble::as_tibble()
-  
-  predictions
 }
 
 #' @importFrom generics augment
