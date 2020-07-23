8c46337b6dc2e1e362f483b435a62beb8ef6cb5f
Simon P. Couch <simonpatrickcouch@gmail.com>
2020-07-23 10:10:05 -0700

    model stack print method (#5)
    will probably want to split up member and class for the classification method.

--- R/print.R
+++ R/print.R
@@ -1,89 +1,22 @@
 #' @export
 print.data_stack <- function(x, ...) {
-  mode <- attr(x, "mode")
   
-  if (mode == "regression") {
-    n_members <- ncol(x) - 1
-    n_model_defs <- length(attr(x, "model_defs"))
-    outcome_name <- colnames(x)[1]
-    submodel_lengths <- purrr::map(attr(x, "cols_map"), length)
-    model_names <- names(attr(x, "cols_map"))
-  } else if (mode == "classification") {
-    n_groups <- length(unique(dplyr::pull(attr(x, "train")[,.get_outcome(x)])))
-    n_members <- (ncol(x) - 1) / n_groups
-    n_model_defs <- length(attr(x, "model_defs"))
-    outcome_name <- colnames(x)[1]
-    submodel_lengths <- 
-      purrr::map_dbl(attr(x, "cols_map"), length) / n_groups
-    model_names <- names(attr(x, "cols_map"))
-  } else {
-    n_members <- 0
-    n_groups <- n_members <- n_model_defs <- submodel_lengths <- 0
-    outcome_name <- model_names <- NULL
-  }
-  
-  cat(glue::glue("# A data stack with {n_model_defs} model definition",
-                 "{if (n_model_defs != 1) 's' else ''}",
-                 " and {n_members} candidate member",
-                 "{if (n_members != 1) 's' else ''}",
-                 "{if (n_model_defs != 0) ':' else '.'}"))
-  if (n_model_defs != 0) {cat("\n")}
-  
-  n_by_model_defs <-
-    purrr::map2(
-      submodel_lengths,
-      model_names,
-      function(submodels, name) {
-        cat(glue::glue(
-          "#   {name}: ",
-          "{submodels} sub-model",
-          "{if (submodels != 1) 's' else ''}")
-        )
-        cat("\n")
-      }
-    )
-  
-  cat(glue::glue(
-    "# Outcome: {if (.get_outcome(x) == 'init_') {NULL} else {.get_outcome(x)}}\n"
-  ))
 }
 
 #' @export
 print.model_stack <- function(x, n = 10, ...) {
+  fitted <- if (is.null(x[["member_fits"]])) {"n unfitted "} else {" fitted "}
   mode <- x[["mode"]]
-  x_ <- x[["data_stack"]]
+  is_reg <- mode == "regression"
   
-  if (mode == "regression") {
-    n_members <- if (ncol(x_) == 0) {0} else {ncol(x_) - 1}
-    n_model_defs <- length(x[["model_defs"]])
-    outcome_name <- colnames(x_)[1]
-    submodel_lengths <- purrr::map(x[["cols_map"]], length)
-    model_names <- names(x[["cols_map"]])
-  } else {
-    n_groups <- length(unique(dplyr::pull(x[["train"]][,.get_outcome(x)])))
-    n_members <- if (ncol(x_) == 0) {0} else {
-      (ncol(x_) - 1) / n_groups
-    }
-    n_model_defs <- length(x[["model_defs"]])
-    outcome_name <- colnames(x_)[1]
-    submodel_lengths <- 
-      purrr::map_dbl(x[["cols_map"]], length) / n_groups
-    model_names <- names(x[["cols_map"]])
-  }
+  intro <- paste0("A", fitted, mode, " model stack.")
+  rlang::inform(intro)
   
-  if (!is.null(x[["member_fits"]])) {
-    cat(glue::glue("# A fitted model stack with {length(x$member_fits)} member",
-                   "{if (length(x$member_fits) != 1) 's:' else ':'}"))
-    cat("\n#   ")
-    cat(paste0(names(x$member_fits), collapse = ", "))
-  } else {
-    cat(glue::glue("# An unfitted model stack with ",
-                   "{n_members} candidate member",
-                   "{if (n_members != 1) 's ' else ' '}",
-                   "from {n_model_defs} model definition",
-                   "{if (n_model_defs != 1) 's.' else '.'}"))
-    cat("\n")
-  }
+  member_summary(x)
+  
+  print_top_coefs(x)
+  
+  invisible(NULL)
 }
 
 top_coefs <- function(x, n = 10) {
@@ -104,22 +37,24 @@ top_coefs <- function(x, n = 10) {
     dplyr::arrange(dplyr::desc(estimate)) 
   
   if (any(names(res) == "class")) {
-    res <- dplyr::select(res, model = model_type, weight = estimate, class, terms)
+    res <- dplyr::select(res, member = terms, type = model_type, weight = estimate)
   } else {
-    res <- dplyr::select(res, model = model_type, weight = estimate)
+    res <- dplyr::select(res, member = terms, type = model_type, weight = estimate)
   }
-res
+  
+  res
 }
 
 print_top_coefs <- function(x, n = 10, digits = 3) {
-  res <- top_coefs(x, n) %>% dplyr::select(-terms)
+  res <- top_coefs(x, n)
   
-  msg <- paste("The", n, "highest weighted sub-models were:")
+  msg <- paste0("The highest weighted member",
+               if (x$mode == "regression") {"s"} else {" classes"},
+               " are:")
   rlang::inform(msg)
   print(res)
   invisible(NULL)
 }
-
 
 member_summary <- function(x) {
   betas <- 
@@ -130,8 +65,8 @@ member_summary <- function(x) {
   used_terms <- nrow(used_betas)
   
   msg <- paste0("Out of ", length(all_terms), " possible candidates, the ",
-                "ensemble contains ", used_terms, " non-zero ", 
-                ifelse(used_terms > 1, "members", "member"), ".")
+                "ensemble contains ", used_terms, 
+                ifelse(used_terms > 1, " members", " member"), ".")
   rlang::inform(msg)
   if (any(names(betas) == "class")) {
     n_classes <- length(unique(betas$class))
@@ -149,6 +84,3 @@ member_summary <- function(x) {
   }
   invisible(NULL)
 }
- 
-
-
