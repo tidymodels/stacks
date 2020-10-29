9bd88d8f0cfbcfc84f6709019052d262fa4f4cb7
Simon P. Couch <simonpatrickcouch@gmail.com>
2020-10-29 10:28:15 -0700

    relax tibble req in constructors (closes #46)
    tbl_df subclasses data.frame, so this should not be breaking. :-)

--- R/constructors.R
+++ R/constructors.R
@@ -2,12 +2,12 @@ data_stack_constr <- function(data_stack) {
   check_inherits(attr(data_stack, "rs_hash"), "character")
   check_inherits(attr(data_stack, "outcome"), "character")
   check_inherits(attr(data_stack, "mode"), "character")
-  check_inherits(attr(data_stack, "train"), "tbl_df")
-  check_inherits(attr(data_stack, "splits"), "tbl_df")
+  check_inherits(attr(data_stack, "train"), "data.frame")
+  check_inherits(attr(data_stack, "splits"), "data.frame")
   
   purrr::map(attr(data_stack, "cols_map"), check_inherits, "character")
   purrr::map(attr(data_stack, "model_defs"), check_inherits, "workflow")
-  purrr::map(attr(data_stack, "model_metrics"), check_inherits, "tbl_df")
+  purrr::map(attr(data_stack, "model_metrics"), check_inherits, "data.frame")
   
   invisible(TRUE)
 }
@@ -15,16 +15,16 @@ data_stack_constr <- function(data_stack) {
 model_stack_constr <- function(model_stack) {
   check_inherits(model_stack[["coefs"]], "model_fit")
   check_inherits(model_stack[["equations"]], "list")
-  check_inherits(model_stack[["train"]], "tbl_df")
-  check_inherits(model_stack[["data_stack"]], "tbl_df")
+  check_inherits(model_stack[["train"]], "data.frame")
+  check_inherits(model_stack[["data_stack"]], "data.frame")
   check_inherits(model_stack[["mode"]], "character")
   check_inherits(model_stack[["outcome"]], "character")
-  check_inherits(model_stack[["splits"]], "tbl_df")
+  check_inherits(model_stack[["splits"]], "data.frame")
   check_inherits(model_stack[["penalty"]], "list")
   
   purrr::map(model_stack[["model_defs"]], check_inherits, "workflow")
   purrr::map(model_stack[["cols_map"]], check_inherits, "character")
-  purrr::map(model_stack[["model_metrics"]], check_inherits, "tbl_df")
+  purrr::map(model_stack[["model_metrics"]], check_inherits, "data.frame")
   purrr::map(model_stack[["member_fits"]], check_inherits, "workflow")
   
   invisible(TRUE)
