5b3006b64a9bb095d6003e835f9af97b2794f715
Simon P. Couch <simonpatrickcouch@gmail.com>
2020-11-12 20:41:27 -0800

    exclude helper data in build, run tests conditionally

--- tests/testthat/helper_functions.R
+++ tests/testthat/helper_functions.R
@@ -19,3 +19,40 @@ check_inherits <- function(x, what) {
   
   invisible(TRUE)
 }
+
+# Helper Data 
+# -----------------------------------------------------------------------
+# the `helper_data.Rda` contains data objects for use in unit testing.
+# due to its size, it's not included in the built package.
+# * when running locally, use the local helper_data. 
+# * when testing on github, download the hard-linked file from the relevant branch.
+# * when on cran, only run the tests in test_cran that don't require the data.
+
+get_current_branch <- function() {
+  gh_ref <- Sys.getenv("GITHUB_REF")
+  
+  if (!identical(gh_ref, "")) {
+    gsub("refs/heads/", "", gh_ref)
+  } else {
+    NA
+  }
+}
+
+on_github <- function() {
+  !is.na(get_current_branch())
+}
+
+on_cran <- function() {
+  !identical(Sys.getenv("NOT_CRAN"), "true")
+}
+
+if (on_github()) {
+  download.file(
+    paste0(
+      "https://github.com/tidymodels/stacks/blob/", 
+      get_current_branch(), 
+      "/tests/testthat/helper_data.Rda?raw=true"
+    ),
+    destfile = paste0(test_path(), "/helper_data.Rda")
+  )
+}
