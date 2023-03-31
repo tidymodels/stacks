tidymodels/stacks

commit b2dad2e29f6c6d695e7b20f1def682fd9d0f0413

transition from deprecated `map_df*()` variants

R/print.R
@@ -82,10 +82,12 @@ top_coefs <- function(x, penalty = x$penalty$penalty, n = 10) {
   n <- min(n, nrow(betas))
   
   sub_models <-
-    purrr::map_dfr(x$cols_map, ~ tibble::tibble(terms = .x), .id = "model_name")
+    purrr::map(x$cols_map, ~ tibble::tibble(terms = .x), .id = "model_name") %>%
+    purrr::list_rbind()
   model_types <- 
     purrr::map(x$model_defs, workflows::extract_spec_parsnip) %>% 
-    purrr::map_dfr(~ tibble::tibble(model_type = class(.x)[1]), .id = "model_name")
+    purrr::map(~ tibble::tibble(model_type = class(.x)[1]), .id = "model_name") %>%
+    purrr::list_rbind()
   res <- 
     dplyr::left_join(betas, sub_models, by = "terms") %>% 
     dplyr::left_join(model_types, by = "model_name") %>% 

DESCRIPTION
@@ -26,7 +26,7 @@ Imports:
     glmnet,
     glue,
     parsnip (>= 1.0.2),
-    purrr (>= 0.3.2),
+    purrr (>= 1.0.0),
     recipes (>= 0.2.0),
     rlang (>= 0.4.0),
     rsample (>= 0.1.1),

R/expressions.R
@@ -156,13 +156,15 @@ stack_predict.lognet_class <- function(x, data, ...) {
 #' @export
 #' @rdname stack_predict
 stack_predict.lognet_prob <- function(x, data, ...) {
-  purrr::map_dfc(x, rlang::eval_tidy, data = data)
+  purrr::map(x, rlang::eval_tidy, data = data) %>%
+    purrr::list_cbind()
 }
 
 
 multi_net_engine <- function(x, data, ...) {
   res <- 
-    purrr::map_dfc(x$.pred, rlang::eval_tidy, data = data) %>% 
+    purrr::map(x$.pred, rlang::eval_tidy, data = data) %>% 
+    purrr::list_cbind() %>%
     multi_net_helper()
 }
 

R/predict.R
@@ -100,12 +100,13 @@ predict.model_stack <- function(object, new_data, type = NULL, members = FALSE,
   if (members) {
     if (type == "class") {
       member_preds <- 
-        purrr::map_dfc(
+        purrr::map(
           names(object[["member_fits"]]),
           parse_member_probs,
           member_preds,
           levels(object[["data_stack"]][[object[["outcome"]]]])
-        )
+        ) %>%
+        purrr::list_cbind()
     }
     res <- dplyr::bind_cols(res, member_preds)
   }

vignettes/articles/workflowsets.Rmd
@@ -314,7 +314,8 @@ member_preds <-
 Now, evaluating the root mean squared error from each model:
 
 ```{r}
-map_dfr(member_preds, rmse, truth = latency, data = member_preds) %>%
+map(member_preds, rmse, truth = latency, data = member_preds) %>%
+  list_rbind() %>%
   mutate(member = colnames(member_preds))
 ```
 

vignettes/basics.Rmd
@@ -403,7 +403,8 @@ member_preds <-
 Now, evaluating the root mean squared error from each model:
 
 ```{r}
-map_dfr(member_preds, rmse, truth = latency, data = member_preds) %>%
+map(member_preds, rmse, truth = latency, data = member_preds) %>%
+  list_rbind() %>%
   mutate(member = colnames(member_preds))
 ```
 

vignettes/classification.Rmd
@@ -244,10 +244,11 @@ tree_frogs_pred <-
 
 tree_frogs_pred
 
-map_dfr(
+map(
   setNames(colnames(tree_frogs_pred), colnames(tree_frogs_pred)),
   ~mean(tree_frogs_pred$reflex == pull(tree_frogs_pred, .x))
 ) %>%
+  list_rbind() %>%
   pivot_longer(c(everything(), -reflex))
 ```
 

commit 4e639f886e7441ceae960c701b1720dd7fd897ab

fix `print()` bug introduced in b2dad2e

R/print.R
@@ -82,12 +82,12 @@ top_coefs <- function(x, penalty = x$penalty$penalty, n = 10) {
   n <- min(n, nrow(betas))
   
   sub_models <-
-    purrr::map(x$cols_map, ~ tibble::tibble(terms = .x), .id = "model_name") %>%
-    purrr::list_rbind()
+    purrr::map(x$cols_map, ~ tibble::tibble(terms = .x)) %>%
+    purrr::list_rbind(names_to = "model_name")
   model_types <- 
     purrr::map(x$model_defs, workflows::extract_spec_parsnip) %>% 
-    purrr::map(~ tibble::tibble(model_type = class(.x)[1]), .id = "model_name") %>%
-    purrr::list_rbind()
+    purrr::map(~ tibble::tibble(model_type = class(.x)[1])) %>%
+    purrr::list_rbind(names_to = "model_name")
   res <- 
     dplyr::left_join(betas, sub_models, by = "terms") %>% 
     dplyr::left_join(model_types, by = "model_name") %>% 